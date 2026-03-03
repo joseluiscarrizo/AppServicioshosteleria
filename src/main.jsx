2397bd3f1fd55b5c95136980dda986223b7e5c3b
Josep Lluis Carrizo <barjupiterbcn@gmail.com>
2026-03-03 04:11:31 +0100

    Fix Vite build by using relative imports in entrypoint.

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.jsx'
-import '@/index.css'
+import App from './App.jsx'
+import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
@@ -17,6 +17,3 @@ if (import.meta.hot) {
     globalThis.parent?.postMessage({ type: 'sandbox:afterUpdate' }, '*');
   });
 }
-
-
-
