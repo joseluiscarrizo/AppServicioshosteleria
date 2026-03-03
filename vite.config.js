ba1f4ab906b1d89ba06e808bab8a3cf09a8af42a
Josep Lluis Carrizo <barjupiterbcn@gmail.com>
2026-03-03 04:05:40 +0100

    Restore full Vite config with base and allowedHosts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,25 @@
 import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   base: './',
-  // other configurations...
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  build: {
+    sourcemap: 'hidden',
+  },
+  server: {
+    host: true,
+    allowedHosts: [
+      'ta-01kjrshcaaaawd98qw17rnz9d8-5173-ruhene2vuqq11bkrsbdcludrh.w.modal.host',
+      'all',
+      '.modal.host',
+      '.w.modal.host',
+    ],
+  },
 });
